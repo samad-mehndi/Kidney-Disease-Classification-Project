52e9a4a9fcdce386949661504c9c9f3375bdab30
Samad Mehndi <syedzaidisam@gmail.com>
2025-05-27 23:39:12 -0500

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-from src.cnnClassifier import logger
+import sys
+import os
 
-logger.info("Logging is set up successfully.")
+# Add the 'src' directory to the path
+sys.path.append(os.path.join(os.getcwd(), 'src'))
+
+from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    pipeline = DataIngestionTrainingPipeline()
+    pipeline.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
